4f155de94bb7bffc4c1ab42e8545c56b09199db2
rohitpahan <rohit.pahan@bigbasket.com>
2021-01-13 00:53:46 +0530

    added one submodules

--- section6/frontend/src/App.js
+++ section6/frontend/src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Edit <code>src/App.js</code> Interesting.. not it is comming from docker-compose.
         </p>
         <a
           className="App-link"
